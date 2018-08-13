c3e689cd95e3597b7fd616bec59b6db18b4be886
TS Planning Engineer <ym5@ualberta.ca>
2018-08-13 11:22:29 -0600

    clean code

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var conString = "";
-var obj = "1";
-var fs = require('fs');
-
-
-function walkfolders(dir) {
-    var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
-    var filelist = [];
-    files.forEach(function(file) {
-            filelist.push(file);
-    });
-    return filelist;
-}
-
 var appName = 'Logsum Model';
 
 router.get('/', function(req, res, next) {
